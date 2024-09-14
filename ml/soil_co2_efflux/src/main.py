88020f13c78748bbf8fcf62f2d48022ca92f9bb5
Ilias Karatsin <hlias.karas@yahoo.gr>
2024-09-14 04:08:09 +0300

    Enhance main with lucas processed data saving

--- ml/soil_co2_efflux/src/main.py
+++ ml/soil_co2_efflux/src/main.py
@@ -1,3 +1,6 @@
+import os
+
+from data.data_writer import DataWriter
 from data.lucas_soil_data_loader import LucasSoilDataLoader
 from preprocessing.lucas_soil_data_cleaning import clean_oc
 from utils.config import Config
@@ -7,3 +10,6 @@ local_soil_data_loader = LucasSoilDataLoader(config)
 data = local_soil_data_loader.load_raw_data_csv()
 
 cleaned_data = clean_oc(data)
+
+data_writer = DataWriter()
+data_writer.save_to_csv(cleaned_data, os.path.join(config.PATHS.PROCESSED_DATA_FILE_PATH, LucasSoilDataLoader.PROCESSED_DATA_FILENAME))

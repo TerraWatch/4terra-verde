cd5dc6b962b421b5a5233f12b9b14b61e67e2268
Ilias Karatsin <hlias.karas@yahoo.gr>
2024-09-14 18:40:35 +0300

    Fix imports

--- ml/soil_co2_efflux/src/soil_co2_efflux/main.py
+++ ml/soil_co2_efflux/src/soil_co2_efflux/main.py
@@ -1,9 +1,9 @@
 import os
 
-from data.data_writer import DataWriter
-from data.lucas_soil_data_loader import LucasSoilDataLoader
-from preprocessing.lucas_soil_data_cleaning import clean_oc
-from utils.config import Config
+from soil_co2_efflux.data.data_writer import DataWriter
+from soil_co2_efflux.data.lucas_soil_data_loader import LucasSoilDataLoader
+from soil_co2_efflux.preprocessing.lucas_soil_data_cleaning import clean_oc
+from soil_co2_efflux.utils.config import Config
 
 config = Config()
 local_soil_data_loader = LucasSoilDataLoader(config)
@@ -12,4 +12,10 @@ data = local_soil_data_loader.load_raw_data_csv()
 cleaned_data = clean_oc(data)
 
 data_writer = DataWriter()
-data_writer.save_to_csv(cleaned_data, os.path.join(config.PATHS.PROCESSED_DATA_FILE_PATH, LucasSoilDataLoader.PROCESSED_DATA_FILENAME))
+data_writer.save_to_csv(
+    cleaned_data,
+    os.path.join(
+        config.PATHS.PROCESSED_DATA_FILE_PATH,
+        LucasSoilDataLoader.PROCESSED_DATA_FILENAME
+    )
+)

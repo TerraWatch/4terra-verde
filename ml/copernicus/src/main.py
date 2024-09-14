f7f9a7c9f536ed5e13d05f742ae8ed0fa82cb34f
Ilias Karatsin <hlias.karas@yahoo.gr>
2024-09-15 01:51:46 +0300

    Enhance main to also update the lucas_soil data with new copernicus metrics

--- ml/copernicus/src/main.py
+++ ml/copernicus/src/main.py
@@ -11,9 +11,9 @@ from soil_co2_efflux.utils.config import Config as SoilCo2EffluxConfig
 from utils.config import Config
 
 soil_co2_efflux_config = SoilCo2EffluxConfig()
-local_soil_data_writer = DataWriter()
-local_soil_data_loader = LucasSoilDataLoader(soil_co2_efflux_config)
-lucas_soil_data = local_soil_data_loader.load_processed_data_csv()
+lucas_soil_data_writer = DataWriter()
+lucas_soil_data_loader = LucasSoilDataLoader(soil_co2_efflux_config)
+lucas_soil_data = lucas_soil_data_loader.load_processed_data_csv()
 #
 spatial_features_generator = SpatialFeaturesGenerator(lucas_soil_data)
 config = Config()
@@ -32,7 +32,7 @@ for feature_type in CopernicusSatelliteMetricType:
 copernicus_metrics_loader = CopernicusMetricsLoader(config)
 ndvi_raw_copernicus_metrics_data = copernicus_metrics_loader.load_raw_data_csv('NDVI_RESULTS_YEAR_MONTH_*.csv')
 
-local_soil_data_writer.add_column_and_save(
+lucas_soil_data_writer.add_column_and_save(
     lucas_soil_data,
     ndvi_raw_copernicus_metrics_data,
     'NDVI',
